e3bbdb737e8906cc837681375b1a31c7458de37f
shivani <shivani@gmail.com>
2015-10-21 16:33:17 +0530

    print result

---
+++ y.c
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the first no");
+scanf("%d"&a);
+printf("enter the second no");
+scanf("%d"&b);
+c=a+b;
+getch();
+}
